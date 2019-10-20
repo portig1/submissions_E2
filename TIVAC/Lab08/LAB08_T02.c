6989ce28a860bd8440992d2f3afba5a89971e603
portig1 <47192938+portig1@users.noreply.github.com>
2019-10-20 14:19:44 -0700

    Lab 8 Upload
    Lab 8 Finished

---
+++ TIVAC/Lab08/LAB08_T02.c
@@ -0,0 +1,118 @@
+/*
+ * main.c
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "driverlib/ssi.h"
+#include "utils/uartstdio.h"
+
+#define NUM_LEDS 1
+uint8_t frame_buffer[NUM_LEDS*3];
+void send_data(uint8_t* data, uint8_t num_leds);
+void fill_frame_buffer(uint8_t r, uint8_t g, uint8_t b, uint32_t num_leds);
+static volatile uint32_t ssi_lut[] = {
+	0b100100100,
+	0b110100100,
+	0b100110100,
+	0b110110100,
+	0b100100110,
+	0b110100110,
+	0b100110110,
+	0b110110110
+};
+
+int main(void) {
+	
+    FPULazyStackingEnable();
+
+    // 80MHz
+    SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ |
+                       SYSCTL_OSC_MAIN);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlDelay(50000);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
+    SysCtlDelay(50000);
+
+    GPIOPinConfigure(GPIO_PA5_SSI0TX);
+    GPIOPinConfigure(GPIO_PA2_SSI0CLK);
+    GPIOPinConfigure(GPIO_PA4_SSI0RX);
+    GPIOPinConfigure(GPIO_PA3_SSI0FSS);
+
+    GPIOPinTypeSSI(GPIO_PORTA_BASE, GPIO_PIN_5);
+    GPIOPinTypeSSI(GPIO_PORTA_BASE, GPIO_PIN_2);
+    GPIOPinTypeSSI(GPIO_PORTA_BASE, GPIO_PIN_4);
+    GPIOPinTypeSSI(GPIO_PORTA_BASE, GPIO_PIN_3);
+    //20 MHz data rate
+    SSIConfigSetExpClk(SSI0_BASE, 80000000, SSI_FRF_MOTO_MODE_0, SSI_MODE_MASTER, 2400000, 9); 
+    SSIEnable(SSI0_BASE);
+
+    uint32_t i = 0;
+   // fill_frame_buffer(255, 0, 0, NUM_LEDS); //R
+    while(1)
+    {
+        fill_frame_buffer(255, 0, 0, NUM_LEDS); //R
+        send_data(frame_buffer, NUM_LEDS);
+        SysCtlDelay(800000); // delay more then 50us
+
+        fill_frame_buffer(0, 255, 0, NUM_LEDS); //G
+        send_data(frame_buffer, NUM_LEDS);
+        SysCtlDelay(800000); // delay more then 50us
+
+    	fill_frame_buffer(0, 0, 255, NUM_LEDS); //B
+    	send_data(frame_buffer, NUM_LEDS);
+    	SysCtlDelay(800000); // delay more then 50us
+
+    	fill_frame_buffer(255, 255, 0, NUM_LEDS); //RG
+    	send_data(frame_buffer, NUM_LEDS);
+    	SysCtlDelay(800000); // delay more then 50us
+
+        fill_frame_buffer(255, 0, 255, NUM_LEDS); //RB
+        send_data(frame_buffer, NUM_LEDS);
+        SysCtlDelay(800000); // delay more then 50us
+
+        fill_frame_buffer(0, 255, 255, NUM_LEDS); //GB
+        send_data(frame_buffer, NUM_LEDS);
+        SysCtlDelay(800000); // delay more then 50us
+
+        fill_frame_buffer(40, 255, 255, NUM_LEDS); //RGB
+        send_data(frame_buffer, NUM_LEDS);
+        SysCtlDelay(800000); // delay more then 50us
+    }
+
+	return 0;
+}
+
+void send_data(uint8_t* data, uint8_t num_leds)
+{
+	uint32_t i, j, curr_lut_index, curr_rgb;
+	for(i = 0; i < (num_leds*3); i = i + 3) {
+		curr_rgb = (((uint32_t)data[i + 2]) << 16) | (((uint32_t)data[i + 1]) << 8) | data[i];
+		for(j = 0; j < 24; j = j + 3) {
+			curr_lut_index = ((curr_rgb>>j) & 0b111);
+			SSIDataPut(SSI0_BASE, ssi_lut[curr_lut_index]);
+		}
+	}
+
+	SysCtlDelay(50000); // delay more then 50us
+}
+
+void fill_frame_buffer(uint8_t r, uint8_t g, uint8_t b, uint32_t num_leds)
+{
+	uint32_t i;
+	uint8_t* frame_buffer_index = frame_buffer;
+	for(i = 0; i < num_leds; i++) {
+		*(frame_buffer_index++) = g;
+		*(frame_buffer_index++) = r;
+		*(frame_buffer_index++) = b;
+	}
+}
